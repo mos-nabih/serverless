b0a3ce9ca283730be3aeb7bb1b0fbc1da96ef2e1
Mostafa Nabih <mostafa.anm91@gmail.com>
2026-06-16 17:55:07 +0200

    Setup local dynamodb via docker

--- services/tasks_api/store.py
+++ services/tasks_api/store.py
@@ -8,11 +8,12 @@ from models import Task, TaskStatus
 
 
 class TaskStore:
-    def __init__(self, table_name):
+    def __init__(self, table_name, dynamodb_url=None):
         self.table_name = table_name
+        self.dynamodb_url = dynamodb_url
 
     def add(self, task):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)
         table = dynamodb.Table(self.table_name)
         table.put_item(
             Item={
@@ -28,7 +29,7 @@ class TaskStore:
         )
 
     def get_by_id(self, task_id, owner):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)
         table = dynamodb.Table(self.table_name)
         record = table.get_item(
             Key={
@@ -44,18 +45,18 @@ class TaskStore:
         )
 
     def list_open(self, owner):
-        return self._list_by_status(owner, TaskStatus.OPEN)
+        return self._list_by_status(owner, "OPEN")
 
     def list_closed(self, owner):
-        return self._list_by_status(owner, TaskStatus.CLOSED)
+        return self._list_by_status(owner, "CLOSED")
 
     def _list_by_status(self, owner, status):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)
         table = dynamodb.Table(self.table_name)
         last_key = None
         query_kwargs = {
             "IndexName": "GS1",
-            "KeyConditionExpression": Key("GS1PK").eq(f"#{owner}#{status.value}"),
+            "KeyConditionExpression": Key("GS1PK").eq(f"#{owner}#{status}"),
         }
         tasks = []
         while True:
