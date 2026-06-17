2dc99767be1951fe511dd14b277bf992d529750e
Mostafa Nabih <mostafa.anm91@gmail.com>
2026-06-17 14:41:55 +0200

    Install amplify vue js for authetication

--- services/tasks_ui/src/App.vue
+++ services/tasks_ui/src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+  import { Authenticator } from '@aws-amplify/ui-vue';
+  import '@aws-amplify/ui-vue/styles.css';
 
+</script>
 <template>
-  <HelloWorld />
+  <authenticator username-alias="email" :login-mechanisms="['email']">
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
 </template>
---
+++ services/tasks_ui/src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsExports = {
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-1_9KMTJ1k2h',
+      userPoolClientId: '2c35v47jnjlpimak20qrcbuk50',
+    }
+  }
+};
+export default awsExports;
